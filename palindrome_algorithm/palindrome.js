18fa53d86b12fc42db820272c3a3bf26356fcba6
major <echolabstech@gmail.com>
2019-05-26 11:55:18 +0800

    add some more instructions

--- palindrome_algorithm/palindrome.js
+++ palindrome_algorithm/palindrome.js
@@ -2,8 +2,8 @@ console.console.log("Hello World");
 
 function palindrome(word) {
 	console.log(word);
+	// if palindrome, return true;
+	// else return false;
 }
 
-if (true) {
-  
-}
+palindrome("racecar");
