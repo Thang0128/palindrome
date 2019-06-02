7c135ca6ad8d350d834b46b9621236d5d43998da
major <echolabstech@gmail.com>
2019-06-02 10:30:58 +0800

    add notes to problem

--- scope_exercises/script.js
+++ scope_exercises/script.js
@@ -1,9 +1,11 @@
 /*
 	1. what happens and why? 
-	2. how do we get the a to print undefined
+	2. how do we get the a to print undefined without changing the if block
 */
+// global or script scope
 function problem1() {
     if(true) {
+    	// local block scope
         var a = 5;
     }
     console.log(a);
