798cb3dbf6dc47f9ab49037549a2af44e29ae78c
major <echolabstech@gmail.com>
2019-06-02 10:14:18 +0800

    start of problem 1

---
+++ scope_exercises/script.js
@@ -0,0 +1,11 @@
+/*
+	1. what happens and why? 
+	2. how do we get the a to print undefined
+*/
+function problem1() {
+    if(true) {
+        var a = 5;
+    }
+    console.log(a);
+}
+problem1();
