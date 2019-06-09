204fc4f4d6db0aa110f2800e8111e5f85159ca2c
major <echolabstech@gmail.com>
2019-06-09 12:41:14 +0800

    add instructions for form behavior

--- palindrome_webservice/scripts/script.js
+++ palindrome_webservice/scripts/script.js
@@ -1,1 +1,7 @@
 console.log('loading scripts...');
+
+/*
+1. get a refefence to the form element in the dom
+2. add a submit listener to the form element reference
+3. prevent default form submission when the form is submitted (i.e. click submit)
+*/
