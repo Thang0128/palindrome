867b08a50fbce74a6042f200c996341f3e1ae7a5
TaeWhan <taewhankim@naver.com>
2019-06-16 12:04:06 +0800

    This is commit to prevent to see if the string/word is good for our palindome web

--- palindrome_webservice/scripts/palindrome_algorithm/palindrome.js
+++ palindrome_webservice/scripts/palindrome_algorithm/palindrome.js
@@ -3,19 +3,29 @@
  test only passes when you implement
  correct logic.
 */
+
+function wordRule(word){
+	let pattern = /[a-zA-Z]{2,}/
+	assert.notEqual(word.match(pattern), null)
+}
+
 function palindrome(word) {
-	let a = word.length;
-	let firChar = 0;
-	let c = parseInt(a/2);
-	for(let i = a; i > c; i--){
-		let endChar = i-1;
-		if (word[firChar] === word[endChar]) {
-			firChar++;
-		}else {
-			return false;
+	if(wordRule !== null){
+		return false;
+	}else{
+		let a = word.length;
+		let firChar = 0;
+		let c = parseInt(a/2);
+		for(let i = a; i > c; i--){
+			let endChar = i-1;
+			if (word[firChar] === word[endChar]) {
+					firChar++;
+			}else {
+				return false;
+			}
 		}
+		return true;
 	}
-	return true;
 }
 
 /*
