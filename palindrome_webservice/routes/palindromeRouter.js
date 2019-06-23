827679e72a5bf4b2e5942d1e18767d9eb807616c
TaeWhan <taewhankim@naver.com>
2019-06-23 13:24:17 +0800

    stub the palindrome data base return to json

--- palindrome_webservice/routes/palindromeRouter.js
+++ palindrome_webservice/routes/palindromeRouter.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const array =  ['dad\n', 'mom\n', 'racecar\n']
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({palindrome: array});
 });
 
 module.exports = router;
